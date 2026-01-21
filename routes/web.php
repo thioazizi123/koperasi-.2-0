aa6fc50b99bd847fdf3abeadc7cf000b337fcdb3
elsae-gif <elsae8852@gmail.com>
2026-01-21 10:56:09 +0700

    M

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,8 @@
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\FinancingController;
 use App\Http\Controllers\ReportController;
-<<<<<<< HEAD
-use App\Models\Member;
-=======
-<<<<<<< HEAD
 use App\Http\Controllers\SavingController;
-=======
->>>>>>> af4bf4565d9417ba6bf8a1095aa9d3cc061bfd9a
->>>>>>> f8dae23d7020d9d7b2de6bf36f5a6fa5b7bdef04
->>>>>>> c7bfbe3cfedfa483dce7b5ca448d27d9e98aab3d
+use App\Models\Member;
 
 Route::get('/', function () {
     $totalMembers = Member::count();
@@ -21,24 +14,14 @@ Route::get('/', function () {
     return view('home', compact('totalMembers', 'newMembers', 'latestMembers'));
 });
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+Route::resource('members', MemberController::class);
+Route::resource('financings', FinancingController::class);
+Route::put('/installments/{installment}/pay', [FinancingController::class, 'payInstallment'])->name('installments.pay');
+
 Route::get('/report', [ReportController::class, 'index'])->name('reports.index');
+
 Route::prefix('simpanan')->group(function () {
     Route::get('/wajib', [SavingController::class, 'wajib'])->name('simpanan.wajib');
     Route::get('/pokok', [SavingController::class, 'pokok'])->name('simpanan.pokok');
     Route::get('/operasional', [SavingController::class, 'operasional'])->name('simpanan.operasional');
 });
-=======
-<<<<<<< HEAD
->>>>>>> c7bfbe3cfedfa483dce7b5ca448d27d9e98aab3d
-Route::resource('members', MemberController::class);
-Route::resource('financings', FinancingController::class);
-Route::put('/installments/{installment}/pay', [FinancingController::class, 'payInstallment'])->name('installments.pay');
-Route::get('/report', [ReportController::class, 'index'])->name('reports.index');
-<<<<<<< HEAD
-=======
->>>>>>> af4bf4565d9417ba6bf8a1095aa9d3cc061bfd9a
->>>>>>> f8dae23d7020d9d7b2de6bf36f5a6fa5b7bdef04
->>>>>>> c7bfbe3cfedfa483dce7b5ca448d27d9e98aab3d
