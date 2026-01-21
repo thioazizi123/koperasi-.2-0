59f3b09fdebe0c7c93d660cf4484eb4c1f47b791
thioazizi123 <thioazizi123@gmail.com>
2026-01-21 10:50:34 +0700

    update simpanan

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\ReportController;
+use App\Http\Controllers\SavingController;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/report', [ReportController::class, 'index'])->name('reports.index');
+Route::prefix('simpanan')->group(function () {
+    Route::get('/wajib', [SavingController::class, 'wajib'])->name('simpanan.wajib');
+    Route::get('/pokok', [SavingController::class, 'pokok'])->name('simpanan.pokok');
+    Route::get('/operasional', [SavingController::class, 'operasional'])->name('simpanan.operasional');
+});
